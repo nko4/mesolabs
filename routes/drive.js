b8ba8ec6c7ed66adc44698e63b450701add8a13f
Toshihiro Shimizu <toshihiro_shimizu@dwango.co.jp>
2013-11-09 14:15:13 +0900

    Add startの有無判定

--- routes/drive.js
+++ routes/drive.js
@@ -1,11 +1,15 @@
 exports.index = function(req, res) {
   var start = req.session.start;
 
-  if (start === "東京" || start.toLowerCase() === "tokyo") {
-    start = "都庁前";
-  } else if (start == "大阪" || start.toLowerCase() === "osaka") {
-    start = "梅田";
-  }
+  if (start) {
+    if (start === "東京" || start.toLowerCase() === "tokyo") {
+      start = "都庁前";
+    } else if (start == "大阪" || start.toLowerCase() === "osaka") {
+      start = "梅田";
+    }
 
-  res.render('drive', {start: start});
+    res.render('drive', {start: start});
+  } else {
+    //TODO: 途中参加した人の処理
+  }
 };
